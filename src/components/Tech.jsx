302f8baa313333a94a106f1e103b6371cccfc04b
vincent munywoki <vincentmunywoki12@gmail.com>
2025-03-19 20:14:16 +0300

    my tech.js

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -11,6 +11,7 @@ const Tech = () => {
     <h1 className="text-3xl text-center font-bold my-10 text-green-600">My Skills and Technologies</h1>
 
     <div className='flex flex-row flex-wrap justify-center gap-10'>
+      
       {technologies.map((technology) => (
         <div className='w-28 h-28' key={technology.name}>
           <BallCanvas icon={technology.icon} />
